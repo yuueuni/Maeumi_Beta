926473c6b0a58428ce46e65cdd90edb4a8517f8f
yoon <yuueuni@gmail.com>
2020-04-04 15:47:01 +0900

    Modify Accounts View.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def account(request):
-    return render(request, 'new_signin.html')
-
 def index(request):
     return render(request, 'index.html')
