082e170d81a2033aad00ff169c91e20935dc9ebc
yoon <yuueuni@gmail.com>
2020-04-05 17:08:11 +0900

    Add path analysis

--- maeumi/urls.py
+++ maeumi/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from accounts import views
 
 urlpatterns = [
+    path('analysis/', include('analysis.urls')),
     path('', views.index),
     path('admin/', admin.site.urls),
 ]
